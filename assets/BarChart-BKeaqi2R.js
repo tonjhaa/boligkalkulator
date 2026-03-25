5973d974e9ce68a5b76be3e38e7dc1d47e137114
Tonje Hårstad <tonharstad@gmail.com>
2026-03-25 21:12:35 +0100

    Updates

---
+++ assets/BarChart-BKeaqi2R.js
@@ -0,0 +1,1 @@
+import{r as a}from"./index-Jxx9xFk9.js";import{C as e,k as o}from"./CartesianChart-Bwl5YOEZ.js";var i=["axis","item"],s=a.forwardRef((r,t)=>a.createElement(e,{chartName:"BarChart",defaultTooltipEventType:"axis",validateTooltipEventTypes:i,tooltipPayloadSearcher:o,categoricalChartProps:r,ref:t}));export{s as B};
