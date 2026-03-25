9c9ee57b5ce9be3d9b9690439eac9d27c8169777
Tonje Hårstad <tonharstad@gmail.com>
2026-03-25 23:04:00 +0100

    Updates

---
+++ assets/BarChart-BwVr-giQ.js
@@ -0,0 +1,1 @@
+import{r as a}from"./index-DjtkrYIC.js";import{C as e,k as o}from"./CartesianChart-C1hhXK6B.js";var i=["axis","item"],s=a.forwardRef((r,t)=>a.createElement(e,{chartName:"BarChart",defaultTooltipEventType:"axis",validateTooltipEventTypes:i,tooltipPayloadSearcher:o,categoricalChartProps:r,ref:t}));export{s as B};
