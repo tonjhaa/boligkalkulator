3f010c8d688bfaba0b1d45e5306e33099464110b
tonjhaa <tonjhaa@users.noreply.github.com>
2026-05-11 12:16:19 +0000

    deploy: d67c274c03426a99b713b804cd99e0ef32556a57

---
+++ assets/EconomyStoreContext-CHDN55tI.js
@@ -0,0 +1,1 @@
+import{j as m,r,u,am as x}from"./index-BmYsU0CL.js";const n=r.createContext("economy");function i({store:t,children:o}){return m.jsx(n.Provider,{value:t,children:o})}function y(t){const o=r.useContext(n),e=(a=>a),s=u(e),c=x(e);return o==="partner"?c:s}export{i as E,y as u};
